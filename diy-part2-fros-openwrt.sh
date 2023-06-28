60e78a9bf2d1327054c160001863b4399a68c6c0
aoeII <79325762+aoeII@users.noreply.github.com>
2023-06-28 19:33:00 +0800

    Create diy-part2-fros-openwrt.sh

---
+++ diy-part2-fros-openwrt.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2-fros-openwrt.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+git clone https://github.com/sirpdboy/sirpdboy-package package/sirpdboy-package
+git clone https://github.com/sirpdboy/luci-app-parentcontrol.git package/luci-app-parentcontrol
+#git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
+#git clone https://github.com/sirpdboy/luci-app-advanced.git package/luci-app-advanced
+git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
+#git clone https://github.com/sirpdboy/luci-app-netdata package/luci-app-netdata
